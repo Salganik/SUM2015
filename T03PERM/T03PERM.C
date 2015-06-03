44ee57c43f03453b9ec697ec4b264b7bdc41cc69
Salganik Aleksandr <aleksandrsalganik@mail.ru>
2015-06-03 18:48:33 +0400

    New Program

---
+++ T03PERM/T03PERM.C
@@ -0,0 +1,82 @@
+/* FILE NAME: T03PERM.C
+ * PROGRAMMER: AS3
+ * DATE 03.06.2015
+ * PURPOSE ......
+ */
+
+#include <stdio.h>
+
+#define N 5
+
+int p[N];
+int Parity = 0;
+
+void Write( int x );
+
+void Swap( int *A, int *B )
+{
+  int tmp = *A;
+
+  *A = *B;
+  *B = tmp;
+}
+
+void Go( int Pos )
+{
+  int i ; 
+
+  if (Pos == N)
+  { 
+    for (i = 0; i < N; i++)
+      Write(p[i]);  
+    for (i = 0; i < N; i++)
+      printf("%i", p[i]);
+    printf(" - %s\n", Parity ? "odd" : "even");
+    return;   
+  }  
+
+  else
+  {
+    for (i = Pos; i < N; i++)
+    {
+      
+      if (Pos != i)
+        Parity = !Parity; 
+      Swap(&p[Pos], &p[i]);
+      Go(Pos + 1);  
+      if (Pos != i)
+        Parity = !Parity; 
+      Swap(&p[Pos], &p[i]);
+         
+    }  
+  }
+  
+}
+
+void Write( int x )
+{
+  FILE *F;
+  static int k = 0;
+
+  if ((F = fopen("a.log", "a")) != NULL)
+  {
+    k++;
+    fprintf(F, "%d ", x);
+
+    if (k % 5 == 0)
+      fprintf(F, "\n");
+      
+    fclose(F);  
+  }
+
+}
+
+
+void main( void )
+{
+  int i;
+
+  for (i = 0; i < N; i++)
+    p[i] = i + 1; 
+  Go(0);
+}
